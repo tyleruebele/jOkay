0cc04a64796604747205cbfc952c91abc8885325
Tyler Uebele <tyler.uebele@bettercarpeople.com>
2013-07-31 21:56:55 -0400

    Move error codes to `errno` and return false on failure
    Trim email of whitespace

--- validate-email.js
+++ validate-email.js
@@ -17,6 +17,7 @@
  * 2007-04-11 original version
  * 2007-04-20 revised
  * 2013-07-28 refactored for ease of use
+ * 2013-07-31 move error codes to `errno` and return false on failure
  * 
  * @link http://data.iana.org/TLD/tlds-alpha-by-domain.txt
  * @link http://www.icann.org/registries/listing.html
@@ -29,9 +30,12 @@
  * @param email      The email address to validate
  * @param beStrict   Whether to require an internet domain
  * @param beStricter Whether to require a known TLD
- * @returns int 0 on success
+ * @returns bool|string false on failure, email address on success
  */
 function validateEmail(email, beStrict, beStricter) {
+    // Trim the email of whitespace
+    email = email.replace(/^\s+|\s+$/g, '');
+    
     // Set defaults for strictness parameters
     beStrict = 'undefined' !== typeof beStrict ? beStrict : true;
     beStricter = 'undefined' !== typeof beStricter ? beStricter : false;
@@ -39,7 +43,8 @@ function validateEmail(email, beStrict, beStricter) {
     // Grab user and host, fails if not exactly one @ character
     var Parts = /^([^@]+)@([^@]+)$/.exec(email);
     if (Parts == null) {
-        return 1;
+        validateEmail.errno = 1;
+        return false;
     }
     var user = Parts[1];
     var domain = Parts[2];
@@ -51,7 +56,8 @@ function validateEmail(email, beStrict, beStricter) {
 
     // test user
     if (user.match(LocalPartRE) == null) {
-        return 2;
+        validateEmail.errno = 2;
+        return false;
     }
 
     // An IP based email address is @[#.#.#.#], that is, an IP in square brackets
@@ -62,15 +68,18 @@ function validateEmail(email, beStrict, beStricter) {
 
     // test domain
     if (domain.match(DomainRE) == null) {
-        return 3;
+        validateEmail.errno = 3;
+        return false;
     }
 
     // if IP, validate IP
     if (Parts = domain.match(DomainIPRE)) {
         if (Parts[1] > 255 || Parts[2] > 255 || Parts[3] > 255 || Parts[4] > 255) {
-            return 4;
+            validateEmail.errno = 4;
+            return false;
         } else {
-            return 0; // Success
+            validateEmail.errno = 0;
+            return email;
         }
     }
 
@@ -85,7 +94,8 @@ function validateEmail(email, beStrict, beStricter) {
             // # Version 2013071400, Last Updated Sun Jul 14 07:07:01 2013 UTC
             TLDRE = /^(AC|AD|AE|AERO|AF|AG|AI|AL|AM|AN|AO|AQ|AR|ARPA|AS|ASIA|AT|AU|AW|AX|AZ|BA|BB|BD|BE|BF|BG|BH|BI|BIZ|BJ|BM|BN|BO|BR|BS|BT|BV|BW|BY|BZ|CA|CAT|CC|CD|CF|CG|CH|CI|CK|CL|CM|CN|CO|COM|COOP|CR|CU|CV|CW|CX|CY|CZ|DE|DJ|DK|DM|DO|DZ|EC|EDU|EE|EG|ER|ES|ET|EU|FI|FJ|FK|FM|FO|FR|GA|GB|GD|GE|GF|GG|GH|GI|GL|GM|GN|GOV|GP|GQ|GR|GS|GT|GU|GW|GY|HK|HM|HN|HR|HT|HU|ID|IE|IL|IM|IN|INFO|INT|IO|IQ|IR|IS|IT|JE|JM|JO|JOBS|JP|KE|KG|KH|KI|KM|KN|KP|KR|KW|KY|KZ|LA|LB|LC|LI|LK|LR|LS|LT|LU|LV|LY|MA|MC|MD|ME|MG|MH|MIL|MK|ML|MM|MN|MO|MOBI|MP|MQ|MR|MS|MT|MU|MUSEUM|MV|MW|MX|MY|MZ|NA|NAME|NC|NE|NET|NF|NG|NI|NL|NO|NP|NR|NU|NZ|OM|ORG|PA|PE|PF|PG|PH|PK|PL|PM|PN|POST|PR|PRO|PS|PT|PW|PY|QA|RE|RO|RS|RU|RW|SA|SB|SC|SD|SE|SG|SH|SI|SJ|SK|SL|SM|SN|SO|SR|ST|SU|SV|SX|SY|SZ|TC|TD|TEL|TF|TG|TH|TJ|TK|TL|TM|TN|TO|TP|TR|TRAVEL|TT|TV|TW|TZ|UA|UG|UK|US|UY|UZ|VA|VC|VE|VG|VI|VN|VU|WF|WS|XN--0ZWM56D|XN--11B5BS3A9AJ6G|XN--3E0B707E|XN--45BRJ9C|XN--80AKHBYKNJ4F|XN--80AO21A|XN--90A3AC|XN--9T4B11YI5A|XN--CLCHC0EA0B2G2A9GCD|XN--DEBA0AD|XN--FIQS8S|XN--FIQZ9S|XN--FPCRJ9C3D|XN--FZC2C9E2C|XN--G6W251D|XN--GECRJ9C|XN--H2BRJ9C|XN--HGBK6AJ7F53BBA|XN--HLCJ6AYA9ESC7A|XN--J1AMH|XN--J6W193G|XN--JXALPDLP|XN--KGBECHTV|XN--KPRW13D|XN--KPRY57D|XN--LGBBAT1AD8J|XN--MGB9AWBF|XN--MGBAAM7A8H|XN--MGBAYH7GPA|XN--MGBBH1A71E|XN--MGBC0A9AZCG|XN--MGBERP4A5D4AR|XN--MGBX4CD0AB|XN--O3CW4H|XN--OGBPF8FL|XN--P1AI|XN--PGBS0DH|XN--S9BRJ9C|XN--WGBH1C|XN--WGBL6A|XN--XKC2AL3HYE2A|XN--XKC2DL3A5EE0H|XN--YFRO4I67O|XN--YGBI2AMMX|XN--ZCKZAH|XXX|YE|YT|ZA|ZM|ZW)$/i;
             if (Parts[Parts.length - 1].match(TLDRE) == null) {
-                return 5;
+                validateEmail.errno = 5;
+                return false;
             }
         } else {
             // check short list, and assume a 2 letter TLD is a valid one
@@ -93,30 +103,36 @@ function validateEmail(email, beStrict, beStricter) {
             TLDRE = /^(aero|asia|arpa|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|post|pro|tel|travel|xxx)$/i;
             if (Parts[Parts.length - 1].length != 2
                 && Parts[Parts.length - 1].match(TLDRE) == null
-                ) {
-                return 5;
+            ) {
+                validateEmail.errno = 5;
+                return false;
             }
         }
 
         for (var i = 0; i < Parts.length; i++) {
             if (Parts[i].length > 63) {
-                return 6;
+                validateEmail.errno = 6;
+                return false;
             }
             // check for hyphens below to be more specific in our error
             if (Parts[i].charAt(0) == '-') {
-                return 7;
+                validateEmail.errno = 7;
+                return false;
             }
             if (Parts[i].charAt(Parts[i].length - 1) == '-') {
-                return 8;
+                validateEmail.errno = 8;
+                return false;
             }
             // even though RFC1035 says start with a letter, we don't always
             if (Parts[i].match(/^[a-zA-Z0-9\-]{1,63}$/) == null) {
-                return 9;
+                validateEmail.errno = 9;
+                return false;
             }
         }
     }
 
-    return 0;
+    validateEmail.errno = 0;
+    return email;
 }
 
 /**
@@ -137,20 +153,14 @@ validateEmail.input = function(Input, beStrict, beStricter) {
     }
 
     Input.className = Input.className.replace(validateEmail.errorClass, '');
-    
-    if ('' == Input.value) {
-        Input.title = '';
+
+    var email = validateEmail(Input.value, beStrict, beStricter);
+    if ('' != Input.value && false === email) {
+        Input.title = validateEmail.errors[validateEmail.errno];
+        Input.className += ' ' + validateEmail.errorClass;
     } else {
-        var isInvalid = validateEmail(Input.value, beStrict, beStricter);
-        if (0 === isInvalid) {
-            Input.title = validateEmail.Errors[0];
-        } else if ('undefined' != typeof validateEmail.Errors[isInvalid]) {
-            Input.title = validateEmail.Errors[isInvalid];
-            Input.className += ' ' + validateEmail.errorClass;
-        } else {
-            Input.title = validateEmail.Errors[validateEmail.Errors.length - 1];
-            Input.className += ' ' + validateEmail.errorClass;
-        }
+        Input.title = '';
+        Input.value = email;
     }
 };
 
@@ -161,18 +171,24 @@ validateEmail.init = function() {
     // Find all specified inputs
     var Inputs = document.querySelectorAll(validateEmail.query);
     for (var i = Inputs.length - 1; i >= 0; i--) {
-        Inputs[i].addEventListener('change', function() {
+        Inputs[i].addEventListener('blur', function() {
             validateEmail.input(this);
         });
     }
 };
 
 /**
+ * Hold the error number produced by validateEmail, or 0 for no error
+ * @type {number}
+ */
+validateEmail.errno = 0;
+
+/**
  * validateEmail response code messages
  * Customized script by altering this before validateEmail is called
  * @type {Array}
  */
-validateEmail.Errors = [
+validateEmail.errors = [
     'Email Validation Passed', // 0
     "Wrong number of '@'",     // 1
     'Invalid Email User',      // 2
